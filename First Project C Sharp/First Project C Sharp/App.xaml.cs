7f5cd1ec6dbac6ad4a6af5ab64751cb40e5c0f01
X I P <ps237772@summacollege.nl>
2023-09-15 08:35:13 +0200

    Upload

---
+++ First Project C Sharp/First Project C Sharp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace First_Project_C_Sharp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
